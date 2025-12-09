cf38272f6ca0c890253cde8cf6403e0d41e908cc
Jacob <jacoblittler2003@gmail.com>
2025-12-08 19:37:22 -0500

    Cleaned up variables that I wont be using. Setup the main to run the game. Implemented more Game logic

--- Game.rb
+++ Game.rb
@@ -2,31 +2,34 @@ require_relative './Board'
 require_relative './Computer'
 
 class Game
-  board = Board.new
-  comp = Computer.new
-  white_pegs = 0
-  black_pegs = 0
-  comp_code = comp.comp_code
+  # board = Board.new
+  # comp = Computer.new
+  # comp_code = comp.comp_code
   
-  pegs = Array.new(10, pegs_map = {black: 0, white: 0})
+  # pegs = Array.new(10, pegs_map = {black: 0, white: 0})
   
   def initialize
     @rounds = 0
   end
 
   def play_game
-    until (@rounds == 10 || check_win == true)
-      play_round(board, make_guess)
+    board = Board.new
+    comp = Computer.new
+    comp_code = comp.comp_code
+    pegs = Array.new(10, pegs_map = {black: 0, white: 0})
+    
+    until (@rounds == 10 || check_win(pegs_hash, round))
+      play_round(board, pegs, @rounds, comp_code)
     end
   end
 
-  def play_round(board, guess)
+  def play_round(board, pegs_hash, round, comp_code)
     puts "Enter in four digits (1-6) to crack the code...
     \n White-correct number. Black-correct number and placement.
     \n You have 9 attempts
-    \n You are on attempt: #{@rounds}"
+    \n You are on attempt: #{round}"
     code = gets.chomp
-    valid = update_board(@rounds, code)
+    valid = update_board(round, code)
 
     if valid
       update_pegs(@rounds, code, comp_code)
@@ -37,16 +40,11 @@ class Game
 
   end
 
-  def make_guess
-    
-  end
-
-  def check_win() #check if the black pegs hash ever reachs 4 that is a win
+  def check_win(pegs_hash, round) #check if the black pegs hash ever reachs 4 that is a win
+    if pegs_hash[round][:black] == 4
+      return true
+    end
     return false
-  end
-
-  def prompt_player
-    
   end
 
   def update_pegs(round, player_code, comp_code)
@@ -54,7 +52,8 @@ class Game
       #black pegs
       if player_code[index] == comp_code[index]
         pegs[round][:black] += 1 
-      elsif (player_code[index].count()) && (player_code[index] != comp_code[index]) #need to check whether a digit of the player code ever appears in the comp code, while also not being in the same index to prevent over counting
+      #white pegs
+      elsif (player_code[index].count(comp_code)) && (player_code[index] != comp_code[index]) #need to check whether a digit of the player code ever appears in the comp code, while also not being in the same index to prevent over counting
         pegs[round][:white] += 1
       end
       true
